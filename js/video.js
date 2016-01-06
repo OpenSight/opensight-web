1dbe78b39a05889981cb76f530d56e770f4c4dce
DonQuiote <song.dxs@gmail.com>
2016-01-06 23:32:53 +0800

    add flash hls demo

---
+++ js/video.js
@@ -0,0 +1,10 @@
+var api = new flashlsAPI(getFlashMovieObject("moviename"));
+
+$(function(){
+  setTimeout(function(){
+    // api.load('http://hzhls01.ys7.com:7886/hcnp/549693311_1_2_1_0_183.136.184.7_6500.m3u8');
+    api.load('http://www.opensight.cn/hls/camera1.m3u8');
+    api.play();
+  }, 1000);
+  // 
+});
