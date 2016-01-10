cdb712da8674b6d1c686048eb42ab58ece98eb0d
DonQuiote <song.dxs@gmail.com>
2016-01-10 23:34:29 +0800

    使用自己的摄像头演示实况

--- js/video.js
+++ js/video.js
@@ -1,8 +1,8 @@
 
 var flashvars = {
   // src: 'http://hzhls01.ys7.com:7886/hcnp/549693311_1_2_1_0_183.136.184.7_6500.m3u8',
-  // src: 'http://www.opensight.cn/hls/camera1.m3u8',
-  src: 'rtmp://hzrtmp02.ys7.com:1935/livestream/498570008_1_1_1_0',
+  src: 'http://www.opensight.cn/hls/camera1.m3u8',
+  // src: 'rtmp://hzrtmp02.ys7.com:1935/livestream/498570008_1_1_1_0',
   plugin_hls: "flashlsOSMF.swf",
   autoPlay: true
 };
