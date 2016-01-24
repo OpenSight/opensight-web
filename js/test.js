13863d34e0b399fac01bd786877d056669e9d2a9
DonQuiote <song.dxs@gmail.com>
2016-01-24 17:28:13 +0800

    测试页面增加停止和跳转时间调整功能

---
+++ js/test.js
@@ -0,0 +1,65 @@
+var starting = false;
+
+var start = function(uuid, timeout){
+  starting = true
+  $('#tip').removeClass('hidden').html('正在启动，请等待...');
+  $.ajax({
+    url: 'http://121.41.72.231:5001/api/ivc/v1/projects/demo/cameras/' + uuid + '/sessions',
+    // async: false,
+    data: {
+      format: 'hls',
+      quality: 'ld',
+      create: true
+    },
+    type: 'POST',
+    success: function(info) {
+      $('#tip').removeClass('hidden').html(timeout/1000 + '秒后跳转');
+      setTimeout(function(){
+        window.location.href = info.url;
+        starting = false;
+      }, timeout);
+    }
+  });
+};
+
+var stop = function(uuid){
+  if (starting === true){
+    $('#tip').removeClass('hidden').html('正在启动，不允许停止。');
+    return;
+  }
+  $('#tip').removeClass('hidden').html('正在停止。。。');
+  $.ajax({
+    url: 'http://121.41.72.231:5001/api/ivc/v1/projects/demo/cameras/' + uuid + '/sessions',
+    async: false,
+    cache: false,
+    data: {
+      start: 0,
+      limit: 10
+    },
+    type: 'GET',
+    success: function(info) {
+      for (var i = info.list.length - 1; i >= 0; i--) {
+        $.ajax({
+          url: 'http://121.41.72.231:5001/api/ivc/v1/projects/demo/cameras/' + uuid + '/sessions/' + info.list[i].session_id,
+          async: false,
+          cache: false,
+          type: 'DELETE'
+        });
+      };
+    }
+  });
+  $('#tip').removeClass('hidden').html('停止完成。');
+};
+
+$(function(){
+  $('#start').click(function(){
+    var uuid = $('#uuid').val();
+    var time = $('#time').val();
+    time = parseInt(time, 10);
+    start(uuid, time);
+  });
+  $('#stop').click(function(){
+    var uuid = $('#uuid').val();
+    stop(uuid);
+  });
+});
