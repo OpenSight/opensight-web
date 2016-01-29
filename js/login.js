060fa43c3065ef50c259229369f80ecba38d083b
DonQuiote <song.dxs@gmail.com>
2016-01-29 22:57:44 +0800

    支持公用登录页

--- js/login.js
+++ js/login.js
@@ -2,6 +2,11 @@ var Login = function(){
   this.url = 'http://localhost/git/opensight/client.html';
   this.api = 'http://121.41.72.231:5001/api/ivc/v1/';
   this.timeInterval = 0;
+
+  var params = this.getUrlParams();
+  if (undefined !== params.page){
+    this.url = params.page;
+  }
 };
 Login.prototype = {
   login: function(u, p, r){
@@ -48,6 +53,33 @@ Login.prototype = {
     }
     return {username: u, password: p};
   },
+  getUrlParams: function(){
+    var href = window.location.href;
+    var start = href.indexOf('?') + 1;
+    if (0 === start){
+      return {};
+    }
+    var stop = href.indexOf('#');
+    if (-1 === stop){
+      stop = href.length;
+    }
+    var seach = href.substring(start, stop);
+    return this.parseStr(seach, '&');
+  },
+  parseStr: function(str, sp){
+    var arr = str.split(sp);
+    var data = {};
+    var idx = 0;
+    for (var i = 0, l = arr.length; i < l; i++){
+      var tmp = arr[i].split('=');
+      if (2 > tmp.length){
+        continue;
+      }
+      data[tmp[0]] = tmp[1];
+      idx++;
+    }
+    return data;
+  }
 };
 
 $(function(){
