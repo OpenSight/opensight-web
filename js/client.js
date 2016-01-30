cc539aab649625d7765035f4c645cebe366e794d
DonQuiote <song.dxs@gmail.com>
2016-01-30 23:24:52 +0800

    add ui-bootstrap and ui-router

---
+++ js/client.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('app', [
+  'ui.router',
+  'ui.bootstrap'
+])
+.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/state1");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('state1', {
+      url: "/state1",
+      templateUrl: "partials/state1.html"
+    })
+    .state('state1.list', {
+      url: "/list",
+      templateUrl: "partials/state1.list.html",
+      controller: function($scope) {
+        $scope.items = ["A", "List", "Of", "Items"];
+      }
+    })
+    .state('state2', {
+      url: "/state2",
+      templateUrl: "partials/state2.html"
+    })
+    .state('state2.list', {
+      url: "/list",
+      templateUrl: "partials/state2.list.html",
+      controller: function($scope) {
+        $scope.things = ["A", "Set", "Of", "Things"];
+      }
+    });
+});
