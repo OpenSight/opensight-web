f4408067060db507a1a71094ffe76da6f4245c17
DonQuiote <song.dxs@gmail.com>
2016-02-04 23:42:18 +0800

    add project page

--- js/client/container.js
+++ js/client/container.js
@@ -1,8 +1,9 @@
 'use strict';
+var api = 'http://121.41.72.231:5001/api/ivc/v1/';
 angular.module('app.controller', []).controller('header', ['$scope', '$rootScope', '$http',function ($scope, $rootScope, $http) {
   $scope.username = 'boss';
   $scope.project = {
-    list: [{name: 'p1'}, {name: 'p2'}, {name: 'p3'}]
+    list: []
   };
   $scope.selected = 'default';
   $rootScope.$on('$stateChangeSuccess', function(e, toState, toParams, fromState, fromParams) {
@@ -13,13 +14,39 @@ angular.module('app.controller', []).controller('header', ['$scope', '$rootScope
     }
   });
 
-  $http.get("http://121.41.72.231:5001/api/ivc/v1/projects", {
+  $http.get(api + "projects", {
   }).success(function(response) {
-    console.log('success');
+    $scope.project = response;
   }).error(function(response, status) {
     console.log('error');
   });
   $scope.$on('responseErrorStart', function(rejection, response, status){
     console.log('responseErrorStart');
   });
+}]).controller('project', ['$scope', '$rootScope', '$http',function ($scope, $rootScope, $http) {
+  $scope.project = $rootScope.$stateParams.project;
+  $scope.boolFalse = false;
+  $scope.boolTrue = true;
+
+  $http.get(api + "projects/" + $scope.project, {
+  }).success(function(response) {
+    $scope.info = response;
+  }).error(function(response, status) {
+    console.log('error');
+  });
+
+  $scope.save = function(){
+    var data = {
+      title: $scope.info.title,
+      media_server: $scope.info.media_server,
+      is_public: $scope.info.is_public,
+      desc: $scope.info.desc,
+      long_desc: $scope.info.long_desc,
+    };
+    $http.put(api + "projects/" + $scope.project, data).success(function(response) {
+      console.log('success');
+    }).error(function(response, status) {
+      console.log('error');
+    });
+  };
 }]);
