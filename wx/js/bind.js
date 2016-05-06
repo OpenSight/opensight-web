33053973098b7729d5d1cd869d6d5c5ca57db953
rick <missyoujojo@163.com>
2016-05-07 02:21:32 +0800

    lala

--- wx/js/bind.js
+++ wx/js/bind.js
@@ -1,49 +1,103 @@
 var Login = function(){
-  this.url = 'http://121.41.72.231/client.html';
-  this.api = 'http://121.41.72.231:5001/api/ivc/v1/';
+  this.url = 'http://www.opensight.cn/wx/';
+  this.api = 'http://api.opensight.cn/api/ivc/v1/wechat/';
   this.timeInterval = 0;
+  this.code = "";
+  this.state = "";
 
   var params = this.getUrlParams();
-  if (undefined !== params.page){
-    this.url = params.page;
+  if (undefined !== params.code){
+      this.code = params.code;
+      if (undefined === params.state)
+          params.state = "1";
+      this.state = params.state;
+      switch (params.state){
+          case "1":
+              this.url += "myProject";
+              break;
+          case "2":
+              this.url += "myCamera";
+              break;
+          case "3":
+              this.url += "myInfo";
+              break;
+          default:
+              alert("unkown state:"+params.state);
+              this.url += "myProject";
+              break;
+      }
+  }else{
+      alert("wrong code!");
   }
 };
+
 Login.prototype = {
-  login: function(u, p, r){
+  login: function(u, p){
     if (undefined === u || "" === u){
       return false;
     }
-    if (undefined === u || "" === u){
+    if (undefined === p || "" === p){
       return false;
     }
+
     if (true === this.logining){
       return false;
     }
-    this.logining = true;
+    //this.logining = true;
     var d = new Date ();
     d.setHours(d.getHours() + 1);
     var e = Math.ceil(d.getTime() / 1000);
+    var data = {username: u, password: sjcl.codec.hex.fromBits(sjcl.misc.pbkdf2(p, "opensight.cn", 100000)), expired: e, code: this.code};
 
-    var data = {username: u, password: p, expired: e};
-    var _this = this;
     $.ajax({
-      url: this.api + 'plaintext_login',
+      url: this.api + 'bindings',
       data: data,
       type: 'POST',
       success: function(json){
-        if (true === r){
-          $.cookie('username', u, {expires: 30});
-          $.cookie('password', p, {expires: 30});
-        }
-        var ui = Base64.encodeURI(JSON.stringify(data));
-        window.location.replace(_this.url + '?jwt=' + json.jwt + '&ui=' + ui);
+          $.cookie('binding_id', json.binding_id, {expires: 30});
+          $.ajax({
+              url: api + 'binding_login',
+              data: {binding_id: json.binding_id, expired: e},
+              type: 'POST',
+              success: function(json2){
+                  $.cookie('jwt', json2.jwt, {expires: 30});
+                  window.location.href = this.url+".html";
+                  //var ui = Base64.encodeURI(JSON.stringify(data));
+                  //window.location.href(_this.url + '?jwt=' + json.jwt + '&ui=' + ui);
+              },
+              error: function() {
+                  alert("binding_login error!");
+              }
+          });
+          //Login.bindLogin();
+        //window.location.replace(_this.url + '?jwt=' + json.jwt + '&ui=' + ui);
       }, 
       error: function() {
-        /* Act on the event */
-        _this.logining = false;
+          alert("bind error!");
       }
     });
     return false;
+  },
+  bindLogin:  function(){
+      var d = new Date ();
+      d.setHours(d.getHours() + 1);
+      var e = Math.ceil(d.getTime() / 1000);
+      var data = {binding_id: this.binding_id, expired: e};
+
+      $.ajax({
+           url: api + 'binding_login',
+           data: data,
+           type: 'POST',
+           success: function(json){
+                $.cookie('jwt', json.jwt, {expires: 30});
+                window.location.href = this.url+".html";
+           //var ui = Base64.encodeURI(JSON.stringify(data));
+           //window.location.href(_this.url + '?jwt=' + json.jwt + '&ui=' + ui);
+           },
+           error: function() {
+                alert("binding_login error!");
+           }
+      });
   },
   get: function(){
     var u = $.cookie('username');
@@ -90,15 +144,13 @@ $(function(){
   if (null !== userinfo){
     $('#username').val(userinfo.username);
     $('#password').val(userinfo.password);
-    $('#remember').prop('checked', true);
   }
 
   $('#form').submit(function(event) {
     /* Act on the event */
     var u = $('#username').val();
     var p = $('#password').val();
-    var r = $('#remember').prop('checked');
-    login.login(u, p, r);
+    login.login(u, p);
     return false;
   });
 });
