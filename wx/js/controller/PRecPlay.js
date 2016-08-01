a6696d38a0c9cfc8a81ced0fdb795418b96c1641
DonQuiote <song.dxs@gmail.com>
2016-08-01 22:51:22 +0800

    format

--- wx/js/controller/PRecPlay.js
+++ wx/js/controller/PRecPlay.js
@@ -1,41 +1,41 @@
-app.register.controller('PRecPlay', ['$rootScope', '$scope', '$http', '$q', '$window', '$stateParams', '$state', function($rootScope, $scope, $http, $q, $window, $stateParams, $state){
-    $scope.precplay = (function () {
-        return {
-            init: function () {
-                $scope.recInfo = $rootScope.pRecInfo;
-                var player = document.getElementById("replayPlayer");
-                $rootScope.RecPlayer = player;
-                player.currentTime = 0;
-                player.pause();
-                player.src = $scope.recInfo.hls;
-                player.load();
-                player.play();
-            },
-            backList: function() {
-                $scope.precplay.stopRec();
-//                $state.go('prec');
-                window.history.back();
-            },
-            stopRec: function(){
-                var player = $rootScope.RecPlayer;
-                if (player!==null && player!==undefined && player.currentTime){
-                    player.currentTime = 0;
-                    player.pause();
-                    player.src="movie.ogg";
-                    player.load();
-                }
-            }
-        };
-    })();
-
-    $scope.destroy = function () {
-        if (undefined !== $scope.aborter) {
-            $scope.aborter.resolve();
-            delete $scope.aborter;
+app.register.controller('PRecPlay', ['$rootScope', '$scope', '$http', '$q', '$window', '$stateParams', '$state', function($rootScope, $scope, $http, $q, $window, $stateParams, $state) {
+  $scope.precplay = (function() {
+    return {
+      init: function() {
+        $scope.recInfo = $rootScope.pRecInfo;
+        var player = document.getElementById("replayPlayer");
+        $rootScope.RecPlayer = player;
+        player.currentTime = 0;
+        player.pause();
+        player.src = $scope.recInfo.hls;
+        player.load();
+        player.play();
+      },
+      backList: function() {
+        $scope.precplay.stopRec();
+        //                $state.go('prec');
+        window.history.back();
+      },
+      stopRec: function() {
+        var player = $rootScope.RecPlayer;
+        if (player !== null && player !== undefined && player.currentTime) {
+          player.currentTime = 0;
+          player.pause();
+          player.src = "movie.ogg";
+          player.load();
         }
+      }
     };
+  })();
 
-    $scope.$on('$destroy', $scope.destroy);
-    $scope.precplay.init();
+  $scope.destroy = function() {
+    if (undefined !== $scope.aborter) {
+      $scope.aborter.resolve();
+      delete $scope.aborter;
+    }
+  };
+
+  $scope.$on('$destroy', $scope.destroy);
+  $scope.precplay.init();
 
 }]);
