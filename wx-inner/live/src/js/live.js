152ff0c8a3988569f36c0047c8bb6c52246b08a5
DonQuiote <song.dxs@gmail.com>
2016-09-06 22:47:03 +0800

    live

--- wx-inner/live/src/js/live.js
+++ wx-inner/live/src/js/live.js
@@ -41,7 +41,7 @@ var api = 'http://api.opensight.cn/api/ivc/v1/projects/' + params.project;
 
 $(function () {
   if (undefined === params.live_show) {
-    $.alert('活动已结束。');
+    showState(0)
     return;
   }
 
@@ -56,30 +56,51 @@ $(function () {
     }
   });
 
+  //初始化分享功能
+  var sh = new Share();
+
   $.ajax({
     url: api + '/live_shows/' + params.live_show,
     type: 'GET',
+    // async: false,
     success: function (info) {
       $('title').html(info.name);
-      $('#show-name').html(info.name);
+      $('#show-name').html(info.desc);
       $('#show-desc').html(info.long_desc);
 
+      if (1 !== info.state) {
+        showState(info.state);
+        return;
+      }
       //启动直播
-      new HlsVideo(info.camera_uuid);
+      new HlsVideo(info.camera_uuid, info.start);
 
       //获取观众数
       new Session(info.camera_uuid);
 
       //获取精彩片段列表
       new RecordEvent(info.camera_uuid, info.start);
+
+      sh.onShare(info.name, info.long_desc);
     },
     error: function () {
-      $.alert('活动已结束。');
+      showState(0)
     }
   });
 
-  //初始化分享功能
+  //初始化评论框
+  new Comment();
 });
+
+var showState = function (state) {
+  state = state || 0;
+  var text = ['活动未启动。', '', '活动暂停中。', '活动已结束。'][state];
+  if ('' === text) {
+    return this;
+  }
+  $('#video-container').html('<p class="text-highlight state">' + text + '</p>');
+  return;
+};
 
 var HlsVideo = function (camera, start_from) {
   this.id = 'video-player';
@@ -118,7 +139,7 @@ HlsVideo.prototype = {
       _t.create();
     });
 
-    $('#record-event-container').on('click', '.record', function(){
+    $('#record-event-container').on('click', '.record', function () {
       $('#switch-replay').addClass('hidden');
       $('#switch-live').removeClass('hidden');
       _t.stop();
@@ -132,17 +153,17 @@ HlsVideo.prototype = {
       url: api + '/cameras/' + this.camera,
       type: 'GET',
       success: function (info) {
-        var bitmap = this.getBitmap(info.quality);
+        var bitmap = this.getBitmap(info.flags, 8);
         var qa = this.parse(bitmap);
         if (0 === qa.length) {
-          $.alert('活动已结束。');
+          showState(0)
           return;
         }
         this.quality = qa[qa.length - 1].value;
         this.create();
       },
       error: function () {
-        $.alert('活动已结束。');
+        showState(0)
       },
       context: this
     });
@@ -188,7 +209,7 @@ HlsVideo.prototype = {
         this.keepalive(info.session_id);
       },
       error: function () {
-        $.alert('活动已结束。');
+        showState(0)
       },
       context: this
     });
@@ -199,7 +220,7 @@ HlsVideo.prototype = {
       '" controls autoplay="autoplay" webkit-playsinline="" width="100%" height="100%" src="' +
       hls +
       '" type="application/x-mpegURL"></video>');
-    var player = document.getElementById('this.id');
+    var player = document.getElementById(this.id);
     player.play();
     player.pause();
     player.play();
@@ -216,16 +237,16 @@ HlsVideo.prototype = {
         this.play(info.hls);
       },
       error: function () {
-        $.alert('活动已结束。');
+        showState(0)
       },
       context: this
     });
     return this;
   },
-  keepalive: function(session_id){
+  keepalive: function (session_id) {
     var _t = this;
     this.stop();
-    this.interval = setInterval(function() {
+    this.interval = setInterval(function () {
       $.ajax({
         url: api + '/cameras/' + _t.camera + '/sessions/' + session_id,
         cache: true,
@@ -233,8 +254,8 @@ HlsVideo.prototype = {
       });
     }, 30000);
   },
-  stop: function(){
-    if (undefined !== this.interval){
+  stop: function () {
+    if (undefined !== this.interval) {
       clearInterval(this.interval);
       this.interval = undefined;
     }
@@ -259,9 +280,9 @@ Tip.prototype = {
   },
   start: function () {
     var _this = this;
-    this.timer = setTimeout(function () {
+    this.timer = setInterval(function () {
       _this.update();
-    }, this.interval * 1000);
+    }, _this.interval * 1000);
     return this;
   },
   update: function () {
@@ -285,7 +306,7 @@ Tip.prototype = {
 
 var Session = function (camera) {
   this.camera = camera;
-  this.interval = 10;
+  this.interval = 10000;
   this.stop();
   this.get();
   return this;
@@ -302,7 +323,7 @@ Session.prototype = {
       complete: function () {
         setTimeout(function () {
           _t.get();
-        }, this.interval);
+        }, _t.interval);
       }
     });
     return this;
@@ -350,6 +371,149 @@ RecordEvent.prototype = {
   add: function () { },
   remove: function () {
     $('#' + this.id).html();
+    return this;
+  }
+};
+
+var Comment = function () {
+  this.input = '#cy-cbox-wrapper';
+  this.bar = '.section-floatbar-wap';
+  this.container = $('#SOHUCS');
+
+  this.init();
+  return this;
+};
+Comment.prototype = {
+  init: function () {
+    var _t = this;
+    setTimeout(function () {
+      if (0 === $(_t.input).length) {
+        _t.init();
+      } else {
+        _t.on();
+      }
+    }, 1000);
+    return this;
+  },
+  on: function () {
+    // $(this.bar).on('click', '.issue-text-wap', this, function(e){
+    //   $(e.data.input).css('display', 'block');
+    //   $(e.data.bar).css('display', 'none');
+    // });
+    // $(this.input).on('click', '.mutual-btn-wap', this, function(e){
+    //   $(e.data.bar).css('display', 'block');
+    //   $(e.data.input).css('display', 'none');
+    // });
+    this.container.find('textarea').focus(function () {
+      $('.live-card').addClass('hidden');
+    }).blur(function () {
+      $('.live-card').removeClass('hidden');
+    });
+    return this;
+  },
+};
+
+var Share = function () {
+  this.url = window.location.href;
+  this.inited = undefined;
+  this.title = undefined;
+
+  this.init();
+  this.on();
+
+  return this;
+};
+Share.prototype = {
+  init: function () {
+    var timestamp = Math.round(new Date().getTime() / 1000);
+    var noncestr = this.url + new Date().getTime().toString();
+    $.ajax({
+      url: 'http://api.opensight.cn/api/ivc/v1/wechat/jsapi_signature',
+      type: 'POST',
+      async: false,
+      data: {
+        timestamp: timestamp,
+        noncestr: noncestr,
+        url: this.url
+      },
+      success: function (info) {
+        wx.config({
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: info.appid, // 必填，公众号的唯一标识
+          timestamp: timestamp, // 必填，生成签名的时间戳
+          nonceStr: noncestr, // 必填，生成签名的随机串
+          signature: info.signature, // 必填，签名，见附录1
+          jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        });
+        var _t = this;
+        wx.ready(function () {
+          _t.inited = true;
+          if (undefined !== _t.title) {
+            _t.onMenuShare();
+          }
+        });
+      },
+      context: this
+    });
+    return this;
+  },
+  on: function () {
+    var _t = this;
+    $('#share').click(function () {
+      _t.showTip();
+    });
+    $('#overlay').click(function () {
+      _t.hideTip();
+    });
+    return this;
+  },
+  onMenuShare: function () {
+    var _t = this;
+    wx.onMenuShareAppMessage({
+      title: this.title, // 分享标题
+      desc: this.desc, // 分享描述
+      link: this.url, // 分享链接
+      imgUrl: 'http://www.opensight.cn/img/play-logo.png', // 分享图标
+      success: function () {
+        // 用户确认分享后执行的回调函数
+        _t.hideTip();
+      },
+      cancel: function () {
+        // 用户取消分享后执行的回调函数
+        _t.hideTip();
+      }
+    });
+    wx.onMenuShareTimeline({
+      title: this.title, // 分享标题
+      link: this.url, // 分享链接
+      imgUrl: 'http://www.opensight.cn/img/play-logo.png', // 分享图标
+      success: function () {
+        // 用户确认分享后执行的回调函数
+        _t.hideTip();
+      },
+      cancel: function () {
+        // 用户取消分享后执行的回调函数
+        _t.hideTip();
+      }
+    });
+    return this;
+  },
+  onShare: function (title, desc) {
+    this.title = title;
+    this.desc = desc;
+    if (true === this.inited) {
+      this.onMenuShare();
+    }
+    return this;
+  },
+  showTip: function () {
+    $('#overlay').addClass('modal-overlay-visible');
+    $('#video-container').hide();
+    return this;
+  },
+  hideTip: function () {
+    $('#overlay').removeClass('modal-overlay-visible');
+    $('#video-container').show();
     return this;
   }
 };
