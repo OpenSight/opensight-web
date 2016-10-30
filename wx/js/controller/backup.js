be6c2dcd08a7760614649431b4b1766efcdddd39
kunkka <song.dxs@gmail.com>
2016-10-30 20:36:03 +0800

    delete event record

--- wx/js/controller/backup.js
+++ wx/js/controller/backup.js
@@ -1,12 +1,17 @@
 app.register.controller('Backup', [
   '$rootScope', '$scope', '$http', '$q', '$window', '$stateParams', '$state', 'dateFactory',
-  function($rootScope, $scope, $http, $q, $window, $stateParams, $state, dateFactory) {
-    $scope.backup = (function() {
+  function ($rootScope, $scope, $http, $q, $window, $stateParams, $state, dateFactory) {
+    $scope.backup = (function () {
       return {
-        init: function() {
+        bDel: false,
+        checked_num: 0,
+        all_checked: false,
+        alert_shown: false,
+        confirm_shown: false,
+        init: function () {
           $scope.backup.url = "http://api.opensight.cn/api/ivc/v1/projects/" + G_ProjectName + '/record/events';
         },
-        query: function() {
+        query: function () {
           $('#ToastTxt').html("正在获取录像列表");
           $('#loadingToast').show();
           $http.get($scope.backup.url, {
@@ -14,32 +19,32 @@ app.register.controller('Backup', [
               start: 0,
               limit: 10
             }
-          }).success(function(response) {
+          }).success(function (response) {
             $scope.filelist = response;
-            setTimeout(function() {
+            setTimeout(function () {
               $('#loadingToast').hide();
             }, 100);
-          }).error(function(response, status) {
+          }).error(function (response, status) {
             $('#ToastTxt').html("获取录像列表失败");
             $('#loadingToast').show();
-            setTimeout(function() {
+            setTimeout(function () {
               $('#loadingToast').hide();
             }, 2000);
           });
         },
-        play: function(item) {
-          if (0 === item.state){
+        play: function (item, bDel) {
+          if (0 === item.state || true == bDel) {
             return;
           }
           $rootScope.pRecInfo = item;
           $state.go('precplay');
         },
-        backProject: function() {
+        backProject: function () {
           if ($scope.Player !== undefined)
             $scope.Player.destroy();
           window.history.back();
         },
-        stopRec: function() {
+        stopRec: function () {
           var player = $rootScope.RecPlayer;
           if (player !== null && player !== undefined && player.currentTime) {
             player.currentTime = 0;
@@ -48,13 +53,13 @@ app.register.controller('Backup', [
             player.load();
           }
         },
-        actionShow: function() {
+        actionShow: function () {
           var mask = $('#mask');
           var weuiActionsheet = $('#weui_actionsheet');
           weuiActionsheet.addClass('weui_actionsheet_toggle');
           mask.show()
             .focus() //加focus是为了触发一次页面的重排(reflow or layout thrashing),使mask的transition动画得以正常触发
-            .addClass('weui_fade_toggle').one('click', function() {
+            .addClass('weui_fade_toggle').one('click', function () {
               hideActionSheet(weuiActionsheet, mask);
             });
 
@@ -63,17 +68,81 @@ app.register.controller('Backup', [
           function hideActionSheet(weuiActionsheet, mask) {
             weuiActionsheet.removeClass('weui_actionsheet_toggle');
             mask.removeClass('weui_fade_toggle');
-            mask.on('transitionend', function() {
+            mask.on('transitionend', function () {
               mask.hide();
-            }).on('webkitTransitionEnd', function() {
+            }).on('webkitTransitionEnd', function () {
               mask.hide();
             })
           }
+        },
+        checkeItem: function (item) {
+          if (true === item.checked) {
+            // item.checked = false;
+            $scope.backup.checked_num++;
+          } else {
+            // item.checked = true;
+            $scope.backup.checked_num--;
+          }
+          if ($scope.backup.checked_num === $scope.filelist.list.length) {
+            $scope.backup.all_checked = true;
+          } else if (0 === $scope.backup.checked_num) {
+            $scope.backup.all_checked = false;
+          }
+        },
+        checkeAll: function (list) {
+          var checked = $scope.backup.all_checked;
+          for (var i = 0, l = list.length; i < l; i++) {
+            list[i].checked = checked;
+          }
+        },
+        del: function () {
+          if (0 === $scope.backup.checked_num) {
+            $scope.backup.alert_shown = true;
+          } else {
+            $scope.backup.confirm_shown = true;
+          }
+        },
+        delItems: function () {
+          $scope.backup.confirm_shown = false;
+          $('#ToastTxt').html("正在删除");
+          $('#loadingToast').show();
+          var succ_num = 0;
+          var err_num = 0;
+          var total = 0;
+          for (var i = 0, l = $scope.filelist.list.length; i < l; i++) {
+            if (true === $scope.filelist.list[i].checked) {
+              total++;
+              var event_id = $scope.filelist.list[i].event_id;
+              $http.delete("http://api.opensight.cn/api/ivc/v1/projects/" + G_ProjectName + '/record/events/' + event_id, {}).success((function (id) {
+                return function(response){
+                  for (var i = 0, l = $scope.filelist.list.length; i < l; i++){
+                    if (id === $scope.filelist.list[i].event_id){
+                      $scope.filelist.list.splice(i, 1);
+                      break;
+                    }
+                  }
+                  succ_num++;
+                };
+              })(event_id)).error(function (response, status) {
+                err_num++;
+                console.log('error');
+              });
+            }
+          }
+          var timer = setInterval(function(){
+            $('#ToastTxt').html('成功' + succ_num + '，失败' + err_num + '。');            
+            if (succ_num + err_num === total){
+              clearInterval(timer);
+              setTimeout(function() {
+                $('#loadingToast').hide();
+              }, 2000);
+            }
+          }, 2000);
         }
       }
     })();
 
-    $scope.destroy = function() {
+    $scope.destroy = function () {
       if (undefined !== $scope.aborter) {
         $scope.aborter.resolve();
         delete $scope.aborter;
